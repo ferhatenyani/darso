f74675ba2622124068ab1da119b13ff8041edb3e
yaniii <ferhatenyani19@gmail.com>
2026-07-20 23:36:52 +0200

    feat(app): network requests to load fonts fix

--- src/remotion/HeroComposition.tsx
+++ src/remotion/HeroComposition.tsx
@@ -20,6 +20,7 @@ import { loadFont } from "@remotion/google-fonts/Inter";
  */
 const { fontFamily: INTER_FAMILY } = loadFont("normal", {
   weights: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
 });
 
 /**
